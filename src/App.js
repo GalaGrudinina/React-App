744cdc8482f43fafa352bb50fc54edd343d6c5e3
GalaGrudinina <gala.grudinina98@gmail.com>
2022-02-24 10:07:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useRef} from "react";
+import PostItem from "./components/PostItem";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+
+function App() {
+const[posts, setPosts]=useState([
+  {id:1, title:'JS', body:'description'},
+  {id:2, title:'JS 1', body:'description'},
+  {id:3, title:'JS 2', body:'description'},
+
+])
+
+const createPost=(newPost)=>{
+setPosts([...posts, newPost])
+}
+
+const removePost=(post)=>{
+setPosts(posts.filter(p=>p.id!==post.id))
+}
+  return (
+    <div className="App">
+     <PostForm create={createPost}/>
+      <PostList remove={removePost} posts={posts} title="the list"/>
+    </div>
+  );
+}
+
+
+
+export default App;
